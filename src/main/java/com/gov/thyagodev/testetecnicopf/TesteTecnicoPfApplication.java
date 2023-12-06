6b4fbdb44abc90cb1ce3b262b19d535d746d136f
Thyago Christian Ramos Martins <thyago.martins@esamcsantos.onmicrosoft.com>
2023-12-06 00:43:23 -0300

    Initial commit

---
+++ src/main/java/com/gov/thyagodev/testetecnicopf/TesteTecnicoPfApplication.java
@@ -0,0 +1,13 @@
+package com.gov.thyagodev.testetecnicopf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTecnicoPfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTecnicoPfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gov/thyagodev/testetecnicopf/TesteTecnicoPfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gov.thyagodev.testetecnicopf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteTecnicoPfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
